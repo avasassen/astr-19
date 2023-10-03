ebc4c7fde55c3cba9a74b5c88841fe25ee379736
avasassen <146787308+avasassen@users.noreply.github.com>
2023-10-03 14:11:02 -0700

    Session 1 Prompt 1 Coding Journal 1

---
+++ Session1-prompt.py
@@ -0,0 +1,20 @@
+# This program will write
+# Ava Sassen's Introduction
+
+# This funtion tells the 
+# computer to print 
+# Ava Sassen's introduction
+def PrintIntroduction():
+    print("My name is Ava Sassen.")
+    print("My favorite movie is 'The Other Guys.'")
+    print("My favorite food is any chocolate without nuts.")
+
+# This defines our main()
+# funtion for our program
+def main():
+    PrintIntroduction()
+
+# When we run the program,
+# this executess first
+if __name__ == "__main__":
+    main()
