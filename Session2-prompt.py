fa255091214fa06d9438e2905b295121f2c80b23
avasassen <146787308+avasassen@users.noreply.github.com>
2023-10-04 18:37:37 -0700

    Session 2 prompt 2 Coding Journal

---
+++ Session2-prompt.py
@@ -0,0 +1,19 @@
+>>> x = 7.65
+>>> y = 3.89
+>>> print(x+y)
+11.540000000000001
+>>> print(type(x+y))
+<class 'float'>
+>>> x = 10
+>>> y = 4
+>>> print(x-y)
+6
+>>> print(type(x-y))
+<class 'int'>
+>>> x = 5
+>>> y = 2.5
+>>> print(x*y)
+12.5
+>>> print(type(x*y))
+<class 'float'>
+>>>
