e4cb544de72e166cbfd0b3afb5e731ba02f92b04
avasassen <146787308+avasassen@users.noreply.github.com>
2023-10-05 16:29:45 -0700

    Session 3 using numpy (two parts)

---
+++ using-numpy.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+x = 1.0     #define a float
+y = 2.0     #define another float
+
+#trigonometry
+print(f"np.sin({x}) = {np.sin(x)}") #sin(x)
+print(f"np.cos({x}) = {np.cos(x)}") #cos(x)
+print(f"np.tan({x}) = {np.tan(x)}") #tan(x)
+print(f"np.arcsin({x}) = {np.arcsin(x)}") #arcsin(x)
+print(f"np.arccos({x}) = {np.arccos(x)}") #arccos(x)
+print(f"np.arctan({x}) = {np.arctan(x)}") #arctan(x)
+print(f"np.arctan2({x},{y}) = {np.arctan2(x,y)}") #arctan2(x,y)
+print(f"np.rad2deg{x}) = {np.rad2deg(x)}")        #convert rad 2 deg
+
+#hyperbolic functions
+print(f"np.sinh({x}) = {np.sinh(x)}") #sinh(x)
+print(f"np.cosh({x}) = {np.cosh(x)}") #cosh(x)
+print(f"np.tanh({x}) = {np.tanh(x)}") #tanh(x)
+print(f"np.arcsinh({x}) = {np.arcsinh(x)}") #arcsinh(x)
+print(f"np.arccosh({x}) = {np.arccosh(x)}") #arccosh(x)
+print(f"np.arctanh({x}) = {np.arctanh(x)}") #arctanh(x)
