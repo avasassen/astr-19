45411be995b4cc8c22452b83476414dc614eebdc
avasassen <146787308+avasassen@users.noreply.github.com>
2023-10-22 14:11:00 -0700

    Basic Python coding template

---
+++ template.py
@@ -0,0 +1,12 @@
+# import commands
+import numpy as np 
+import sys
+
+# define the main function
+def main():
+	print('Our main function')
+
+# execution idiom, tells python how and what to run the program
+if __name__ == "__main__":
+	main()     # tells python to execute main() as the first
+# step of the program
